8e55ecb1dac8fd5716cf955a1febf082356b1fd3
Rebb <rebbmarc@gmail.com>
2019-01-07 00:01:42 +0800

    Initial commit

---
+++ app/src/main/java/com/sotto/rebb/constraintLab.java
@@ -0,0 +1,13 @@
+package com.sotto.rebb;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class constraintLab extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_constraint_lab);
+    }
+}
